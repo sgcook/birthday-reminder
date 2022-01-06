d2ae841a9ddddfd9124c346f49f9f8f737c9524c
Sara Cook <saracook087@gmail.com>
2022-01-06 17:57:56 -0500

    Added remove button to each individual person.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import List from "./List";
 function App() {
   const [people, setPeople] = useState(data);
 
+  const removePerson = (id) => {
+    const newPerson = people.filter((person) => person.id !== id);
+    setPeople(newPerson);
+  };
+
   return (
     <main>
       <section className="container">
         <h3>{people.length} birthdays today</h3>
-        <List people={people} />
+        <List people={people} removePerson={removePerson} />
         <button onClick={() => setPeople([])}> Clear All </button>
       </section>
     </main>
